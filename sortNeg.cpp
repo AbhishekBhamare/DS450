f4b6b1cdf88587468e46d0bca3803ddc6696c636
AbhishekBhamare <66770654+AbhishekBhamare@users.noreply.github.com>
2020-11-10 14:01:54 +0530

    Create sortNeg.cpp

---
+++ sortNeg.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+#define ll long long 
+#define mp make_pair
+#define pb push_back
+#define vt vector
+
+
+void solve(){
+
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0;i<n;i++){ 
+      scanf("%d",&arr[i]);
+  }
+  sort(arr,arr+n);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+}
+
+int main()
+{
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r", stdin);
+  freopen("output.txt","w", stdout);
+  #endif
+      // int t;
+      // scanf("%d",&t);
+      // while(t--){
+      solve();
+  return 0;
+}
