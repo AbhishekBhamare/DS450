edfb3e72c3f434a989c587c7a38561da77a6c1bf
AbhishekBhamare <66770654+AbhishekBhamare@users.noreply.github.com>
2021-03-02 00:49:16 +0530

    Create Number of 1 Bits.cpp

---
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int setBits(int N)
+    {
+        int ans=0;
+        while(N>0){
+            if(N&1>0)
+               ++ans;
+            
+            N=N>>1;
+        }
+        return ans;
+    }
+  
+  
+  /* 
+  Using Built-in function:-
+  
+  1. return __builtin_popcount(N);
+  2. return bitset<32>(N).count();
+  
+  */
